93ffcf8e64f56c2ccfcec04d19b9b6fe54230cbf
Kenia Valladarez <keniavalladarez@gmail.com>
2017-10-21 23:53:37 -0400

    Adds project dependecies and configures Express middleware.

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var methodOverride = require('method-override');
+var config = require('./app/config/config');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -18,9 +22,12 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
 
 app.use('/', index);
 app.use('/users', users);
