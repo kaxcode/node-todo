88da4fdc9c1b11cc115dabe44356593d70111428
Kenia Valladarez <keniavalladarez@gmail.com>
2017-10-23 22:57:02 -0400

    Creates the api routes that will be tested.

---
+++ app/router/router.js
@@ -0,0 +1,14 @@
+var Todo = require('../models/Todo.js');
+var TodoController = require('../controllers/todoController')(Todo);
+
+module.exports = function(app){
+
+    app.get('/api/todos', TodoController.GetTodo);
+
+    app.post('/api/todos', TodoController.PostTodo);
+
+    app.put('/api/todos/:todo_id', TodoController.UpdateTodo);
+
+    app.delete('/api/todos/:todo_id', TodoController.DeleteTodo);
+
+}
