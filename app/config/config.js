c6db51873b3d6adcc2572032bd89af6b9c608699
Kenia Valladarez <keniavalladarez@gmail.com>
2017-10-23 22:55:19 -0400

    Refactors.

--- app/config/config.js
+++ app/config/config.js
@@ -3,5 +3,6 @@ var config = {
     db: process.env.MONGOLAB_URI || "mongodb://localhost/nodetodo",
     test_port: 2001,
     test_db: "mongodb://localhost/nodetodo_test"
-}
+};
+
 module.exports = config;
