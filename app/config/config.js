e5962903715775e0b07da3a562b7e1bb0904163d
Kenia Valladarez <keniavalladarez@gmail.com>
2017-10-31 20:38:35 -0400

    Recreating api testing to just use mocha, chai and codecov.

--- app/config/config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    port: process.env.PORT || 2000,
-    db: process.env.MONGOLAB_URI || "mongodb://localhost/nodetodo",
-    test_port: 2001,
-    test_db: "mongodb://localhost/nodetodo_test"
-};
-
-module.exports = config;
--- app/router/router.js
+++
@@ -1,8 +0,0 @@
-var Todo = require('../models/Todo');
-var TodoController = require('../controllers/todoController')(Todo);
-
-module.exports = function(app){
-
-	app.get('/api/todos', TodoController.GetTodo);
-
-}
