79884b1c03d620d06ff762600bc766c5c11f7eca
Kenia Valladarez <keniavalladarez@gmail.com>
2017-11-02 23:34:32 -0400

    Updates mongodb uri

--- app/config/config.js
+++ app/config/config.js
@@ -1,8 +1,8 @@
 var config = {
     port: process.env.PORT || 2000,
-    db: process.env.MONGOLAB_URI || "mongodb://localhost/nodetodo",
+    db: process.env.MONGOLAB_URI || "mongodb://localhost:27017/node-todo",
     test_port: 2001,
-    test_db: "mongodb://localhost/nodetodo_test"
+    test_db: "mongodb://localhost/node-todo-test"
 };
 
 module.exports = config;
