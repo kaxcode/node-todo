298ab433107f741e044443eefc0170803636a72e
Kenia Valladarez <keniavalladarez@gmail.com>
2017-11-05 11:44:40 -0500

    Restructures todo controller.

--- app/controllers/todoController.js
+++ app/controllers/todoController.js
@@ -1,30 +1,13 @@
-"use strict";
+const mongoose = require('mongoose');
+const Todo = mongoose.model('Todo');
 
-var TodoCtrl = function(Todo){
-
-	var TodoObj = {};
-	TodoObj.PostTodo = function(req, res, next){
-		var newTodo = new Todo(req.body);
-		newTodo.save(function(err, todo){
-			if(err){
-				res.json({status: false, error: err.message});
-				return;
-			}
-			res.json({status: true, todo: todo});
-		});
-	}
-
-	TodoObj.GetTodo = function(req, res, next){
-		Todo.find(function(err, todos){
-			if(err) {
-				res.json({status: false, error: "Something went wrong"});
-				return
-			}
-			res.json({status: true, todo: todos});
-		});
-	}
-
-	return TodoObj;
+exports.addTodo = (req, res) => {
+    const todo = new Todo(req.body);
+    newTodo.save(function(err, todo){
+        if(err){
+            res.json({status: false, error: err.message});
+            return;
+        }
+        res.json({message: "API successfully added!", todo});
+    });
 }
-
-module.exports = TodoCtrl;
