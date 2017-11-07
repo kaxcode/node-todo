42b194147cec348f1c95bf996413078ae102ef5e
Kenia Valladarez <keniavalladarez@gmail.com>
2017-11-06 22:27:54 -0500

    Adds the update, delete, and get by id routes and methods.

--- app/controllers/todoController.js
+++ app/controllers/todoController.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 const Todo = mongoose.model('Todo');
 
-exports.getTodo = (req, res) => {
+exports.getTodos = (req, res) => {
     let query = Todo.find({});
     query.exec((err, todos) => {
         if(err) res.send(err);
@@ -14,5 +14,28 @@ exports.addTodo = (req, res) => {
     const todo = new Todo(req.body);
     todo.save(function(err, todo){
         return err ? res.send(err) : res.json({message: "Todo successfully added!", todo});
+    });
+}
+
+exports.getTodo = (req, res) => {
+    Todo.findById(req.params.id, (err, book) => {
+        if(err) res.send(err);
+        res.json(book);
+    });     
+}
+
+exports.deleteTodo = (req, res) => {
+    Todo.remove({_id : req.params.id}, (err, result) => {
+        res.json({ message: "Todo successfully deleted!", result });
+    });
+}
+
+exports.updateTodo= (req, res) => {
+    Todo.findById({_id: req.params.id}, (err, todo) => {
+        if(err) res.send(err);
+        Object.assign(todo, req.body).save((err, todo) => {
+            if(err) res.send(err);
+            res.json({ message: 'Todo updated!', todo });
+        }); 
     });
 }
--- app/router/router.js
+++ app/router/router.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 const todoController = require('../controllers/todoController');
 
-router.get('/api/todos', todoController.getTodo);
+router.get('/api/todos', todoController.getTodos);
 
 router.post('/api/todos', todoController.addTodo);
+
+router.get('/api/todo/:id', todoController.getTodo);
+
+router.delete('/api/todo/:id', todoController.deleteTodo);
+
+router.put('/api/todo/:id', todoController.updateTodo);
 
 module.exports = router;
